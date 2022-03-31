50e4009484e9c36b0bad191130a6463f2f51698a
Muhammad Shafaf <shafaf@zennode.com>
2022-03-31 18:12:27 +0530

    Drag and Drop functionality completed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,37 @@ class App extends React.Component {
 	 state = initialData;
 
 	 onDragEnd = result => {
-	 	// TODO : reorder our column.
+	 	const { destination, source, draggableId } = result;
+
+	 	if(!destination){
+	 		return;
+	 	}
+	 	if (
+	 		destination.droppableId === source.droppableId &&
+	 		destination.index === source.index
+	 		){
+	 		return;
+	 	}
+
+	 	const column = this.state.columns[source.droppableId];
+	 	const newTaskIds = Array.from(column.taskIds);
+	 	newTaskIds.splice(source.index, 1);
+	 	newTaskIds.splice(destination.index, 0, draggableId);
+
+	 	const newColumn = {
+	 		...column,
+	 		taskIds : newTaskIds,
+	 	}
+
+	 	const newState = {
+	 		...this.state,
+	 		columns : {
+	 			...this.state.column,
+	 			[newColumn.id] : newColumn
+	 		}
+	 	}
+
+	 	this.setState(newState);
 	 }
 
 	render() {
@@ -18,7 +48,6 @@ class App extends React.Component {
 				{this.state.columnOrder.map(columnId => {
 					const column = this.state.columns[columnId];
 					const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-
 
 					return <Column key={column.id} column={column} tasks={tasks}/>
 				})}
@@ -29,3 +58,20 @@ class App extends React.Component {
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
